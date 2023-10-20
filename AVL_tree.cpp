1a95762ae40ff706a5ad9c22aa4ff99af08b8a5b
Ilyab <an1mepacan@yandex.ru>
2023-10-20 15:39:58 +0300

    ~~~balance

--- AVL_tree.cpp
+++ AVL_tree.cpp
@@ -5,17 +5,19 @@ using namespace std;
 
 
 struct Node{
-    int val;
+    int val, diff;
     Node* p, *left, *right;
     Node() 
      : p(nullptr)
      , left(nullptr)
-     , right(nullptr){}
+     , right(nullptr)
+     , diff(0){}
     Node(int x) 
      : p(nullptr)
      , left(nullptr)
      , right(nullptr)
-     , val(x){}
+     , val(x)
+     , diff(0){}
     ~Node(){}
 };
 //find, erase, insert
@@ -24,14 +26,14 @@ struct Node{
 class Binary_tree{
     void RotateRight(Node* u, Node* v);
     void RotateLeft(Node* u, Node* v);
+    void updateBalance(Node* u);
+    void reBalance(Node* u);
     Node* head;
-    vector<Node*> diff;
 public:
     Binary_tree() : head(new Node){}
     ~Binary_tree(){
         //we need to delete all nodes
     }
-
     bool find(int x, Node* ptr){
         if(ptr){
             if(x == ptr->val) return true;
@@ -45,25 +47,80 @@ public:
     void insert(int x){
         Node* tmp = new Node(x);
         Node* curr = head;
-        while(curr->right || curr->left){
-            if(curr->val <= x) curr = curr->left;
-            else curr = curr->right;
+        while(curr){
+            if(curr->val < x){
+                if(curr->right) curr = curr->right;
+                else{
+                    curr->right = tmp;
+                    tmp->p = curr;
+                }
+            }
+            else{
+                if(curr->left) curr = curr->left;
+                else{
+                    curr->left = tmp;
+                    tmp->p = curr;
+                }
+            }
         }
-        if(curr->val <= x) curr->left = tmp;
-        else curr->right = tmp;
     }
 private:
     //combination of this two => big rotations
     void RotateRight(Node* u, Node* v){
         u->left = v->right;
         v->right = u;
-        //change diff
+        //change diff  
     }
     void RotateLeft(Node* u, Node* v){
         u->right = v->left;
         v->left = u;
         //change diff
     }
+    void updateBalance(Node* u){//helper func for reBalance
+        if(u->diff < -1 || u->diff > 1){
+            reBalance(u);
+            return;
+        }
+        if(u->p){
+            if((u->p)->left == u) (u->p)->diff++;
+            else if((u->p)->right == u) (u->p)->diff--;
+            if((u->p)->diff != 0) updateBalance(u->p);
+        }
+    }
+    void reBalance(Node* u){
+        if(u->diff == -2){//diff == -2 => right tree deeper than left tree => we will do left turn
+            if(u->right){
+                if(u->right->diff == -1){
+                    RotateLeft(u, u->right);
+                    u->diff = 0;
+                    u->right->diff = 0;
+                    return;
+                }
+                else if(u->right->diff == 0){
+                    RotateLeft(u, u->left);
+                    u->diff = -1;
+                    u->right->diff = 1;
+                }
+                else if()//далее большие повороты
+            }
+        }
+        else if(u->diff == 2){
+            //ПОСЧТИТАТЬ ВРАЩЕНИЯ
+            if(u->left){
+                if(u->left->diff == -1){
+                    RotateLeft(u, u->right);
+                    u->diff = 0;
+                    u->right->diff = 0;
+                    return;
+                }
+                else if(u->left->diff == 0){
+                    RotateRight(u, u->left);
+                    u->diff = -1;
+                    u->right->diff = 1;
+                }
+            }
+        }
+    } 
 
 };
 
